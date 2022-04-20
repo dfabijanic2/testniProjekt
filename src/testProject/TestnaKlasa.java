915f7b871ec2b1d8aebd7dac31ae7cb60a99bb14
dfabijanic <dora.fabijanic@vuv.hr>
2022-04-20 08:34:33 +0200

    commit

--- src/testProject/TestnaKlasa.java
+++ src/testProject/TestnaKlasa.java
@@ -3,5 +3,6 @@ package testProject;
 public class TestnaKlasa {
 	public static void main(String[] args) {
 		 System.out.println("Testiranje instalacije je uspjesno!");
+		 System.out.println("Testiranje instalacije je uspjesno!");
 		 }
 }
